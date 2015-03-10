ba6c721eac2f0eb142e9006b50a1e61386cb7c8e
Jacob Stair <jacob.stair@gmail.com>
2015-03-10 16:09:55 -0400

    Added GeneratedMeme.php functionality.

---
+++ app/Data/Models/GeneratedMeme.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Data\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @class
+ * @author Jacob Stair
+ * @brief _____
+ * @description _____
+ */
+class GeneratedMeme extends Model
+{
+    protected $table = 'generatedmemes';
+
+    protected $guarded = ['id'];
+
+    public static $rules = [];
+}//end class GeneratedMeme
+
+//end file: ./app/Data/Models/GeneratedMeme.php
